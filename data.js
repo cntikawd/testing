66b43a7aaeec1b5b7708534e177e3ec12a9b11db
cntikawd <167682642+cntikawd@users.noreply.github.com>
2024-06-15 04:35:45 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,1 @@
+var data = '<script>function showAlerts() { "R-U-D-I".split("-").forEach((l, i) => setTimeout(() => alert(l), i * 1000)); }</script><button onclick="showAlerts()">Tampilkan Alert</button>';
